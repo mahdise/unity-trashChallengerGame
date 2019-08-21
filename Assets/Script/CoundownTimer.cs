03c0c4902d0fe4a5d49d0346dc1c50d29018f2db
mahdise <mahdiislam12@gmail.com>
2019-08-21 20:45:01 +0200

    all the assets are here

---
+++ Assets/Script/CoundownTimer.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class CoundownTimer : MonoBehaviour
+{
+   public static float currentTime = 1000f;
+    public static float stratingTime = 0f;
+    //public static float startingTime2 = 500f;
+
+  
+
+
+    [SerializeField] Text countdownText;
+
+
+    void strat()
+    {
+
+      
+        
+     
+        
+            currentTime = stratingTime;
+
+                
+      
+
+    }
+   void Update()
+    {
+        
+            currentTime -= 10 * Time.deltaTime;
+        
+   
+
+        
+
+
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+
+            currentTime = 0;
+            GameOver();
+
+        }
+       
+
+           else if (Input.GetKey(KeyCode.M))
+        {
+
+            currentTime += 2000 * Time.deltaTime;
+
+        }
+
+            void GameOver()
+        {
+
+
+            Cursor.visible = true;
+            Screen.lockCursor = false;
+            SceneManager.LoadScene("gameOver_Scene");
+
+
+        }
+    }
+}
---
+++ Assets/Script/TestObjectCollisionScript.cs
@@ -0,0 +1,16 @@
+﻿//help from youtube tutorial
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObjectCollisionScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision with the box has happened");
+        CoundownTimer.currentTime += 100;
+    
+
+       Destroy(gameObject);
+    }
+}
